0d63d4a69d5fdbc9ce12422ef40eb5b52e093ea3
andoshin11 <shinglish11@gmail.com>
2020-04-08 12:00:35 +0900

    Add Location type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,6 @@ import _ts from 'typescript'
 
 export * from './diagnostics'
 
+export type Location = { line: number; character: number }
+
 export type FileEntry = Map<string, { version: number; scriptSnapshot: _ts.IScriptSnapshot }>
