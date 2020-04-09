904df743fcbfc13ecb707aca460d885f678e9b47
andoshin11 <shinglish11@gmail.com>
2020-04-10 03:19:28 +0900

    Update template transformer unit test

--- src/transformers/template/transformers.test.ts
+++ src/transformers/template/transformers.test.ts
@@ -1,41 +1,38 @@
-// import ts from 'typescript'
-// import { createSourceFile } from '../../helpers'
-// import { Printer } from '../../printer'
-// import { transformRenderCall } from './transformers'
+import { transformTemplate } from './transformers'
+import { CompiledVueTemplateFileName } from '../../types'
 
-// describe('transformers/template/transformers', () => {
-//   const printer = new Printer(ts)
+describe('transformers/template/transformers', () => {
 
+  test('transformTemplate', () => {
+    const cases = [
+      `
+import { toDisplayString as _toDisplayString, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
 
-//   test('transformTemplate', () => {
-//     const rawText = `
-// import { toDisplayString as _toDisplayString, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
+export function render(_ctx, _cache) {
+  return (_openBlock(), _createBlock("div", null, [
+    _createVNode("h1", null, _toDisplayString(_ctx.msg), 1 /* TEXT */)
+  ]))
+}
+    `,
+    `
+import { createVNode as _createVNode, toDisplayString as _toDisplayString, resolveComponent as _resolveComponent, openBlock as _openBlock, createBlock as _createBlock } from "vue"
 
-// export function render(_ctx, _cache) {
-//   return (_openBlock(), _createBlock("div", null, [
-//     _createVNode("h1", null, _toDisplayString(_ctx.msg), 1 /* TEXT */)
-//   ]))
-// }
-//     `
-//     const sourceFile = createSourceFile(ts, rawText, ts.ScriptTarget.ES2017)
-//     const output = transformRenderCall(sourceFile, ts)
+function render(_ctx: __ComponentType) {
+  const _component_HelloWorld = _resolveComponent("HelloWorld")
 
-//     expect(printer.print(output)).toMatchSnapshot()
-//   })
+  return (_openBlock(), _createBlock("div", null, [
+    _createVNode("img", { src: "./logo.png" }),
+    _createVNode("h1", null, "Hello Vue 3!"),
+    _createVNode("button", { onClick: _ctx.inc }, "Clicked " + _toDisplayString(_ctx.count.length) + " times.", 9 /* TEXT, PROPS */, ["onClick"]),
+    _createVNode(_component_HelloWorld)
+  ]))
+}   
+    `,
+    ]
 
-//   test('transformRenderCall', () => {
-//     const rawText = `
-// import { toDisplayString as _toDisplayString, createVNode as _createVNode, openBlock as _openBlock, createBlock as _createBlock } from "vue";
-
-// export function render(_ctx, _cache) {
-//   return (_openBlock(), _createBlock("div", null, [
-//     _createVNode("h1", null, _toDisplayString(_ctx.msg), 1 /* TEXT */)
-//   ]))
-// }
-//     `
-//     const sourceFile = createSourceFile(ts, rawText, ts.ScriptTarget.ES2017)
-//     const output = transformRenderCall(sourceFile, ts)
-
-//     expect(printer.print(output)).toMatchSnapshot()
-//   })
-// })
+    cases.forEach(c => {
+      const transformed = transformTemplate('test.vue.template.ts' as CompiledVueTemplateFileName, c)
+      expect(transformed).toMatchSnapshot()
+    })
+  })
+})
