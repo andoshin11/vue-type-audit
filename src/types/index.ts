ba4edb1e08116c1e92a89f0f637f4d550dae1c50
andoshin11 <shinglish11@gmail.com>
2020-04-10 01:59:02 +0900

    Update common types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,13 @@
 import _ts from 'typescript'
+import { RawSourceMap } from 'source-map'
 
 export * from './diagnostics'
+export * from './path'
 
-export type Location = { line: number; character: number }
+export type Location = { line: number; column: number }
 
 export type FileEntry = Map<string, { version: number; scriptSnapshot: _ts.IScriptSnapshot }>
+
+export type SourcemapEntry = Map<string, { map?: RawSourceMap }>
+
+export type Brand<V, Brand extends string> = V & { readonly __brand__: Brand }
