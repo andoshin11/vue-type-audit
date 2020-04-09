e6a91d0215ef56c12fb83d52b175203ef33cc4fb
andoshin11 <shinglish11@gmail.com>
2020-04-10 03:12:27 +0900

    Add proxyService

---
+++ src/proxyService.ts
@@ -0,0 +1,42 @@
+import _ts from 'typescript'
+import * as fs from 'fs'
+import * as path from 'path'
+import { FileEntry, SourcemapEntry } from './types'
+import { createHost, createService } from './languageService'
+import { Printer } from './printer'
+import { Reporter } from './reporter'
+
+export class ProxyService {
+  private tsService: _ts.LanguageService
+
+  printer: Printer
+  reporter: Reporter
+  scriptVersions: FileEntry = new Map()
+  sourcemapEntry: SourcemapEntry = new Map()
+
+  constructor(public fileNames: string[], private compilerOptions: _ts.CompilerOptions, ts: typeof _ts) {
+    this.printer = new Printer(ts)
+    const host = createHost(fileNames, compilerOptions, this.scriptVersions, this.sourcemapEntry, this.printer, ts)
+    this.tsService = createService(host, ts)
+    this.reporter = new Reporter(this.sourcemapEntry)
+  }
+
+  static fromConfigFile(configPath: string, ts: typeof _ts): ProxyService {
+    const content = fs.readFileSync(configPath).toString();
+    const parsed = ts.parseJsonConfigFileContent(
+        JSON.parse(content),
+        ts.sys,
+        path.dirname(configPath)
+    );
+    const compilerOptions = parsed.options
+    const fileNames = parsed.fileNames
+
+    return new ProxyService(fileNames, compilerOptions, ts)
+  }
+
+  getSemanticDiagnostics() {
+    const { tsService } = this
+    const result = tsService.getProgram()!.getSemanticDiagnostics()
+    return [...result]
+  }
+}
