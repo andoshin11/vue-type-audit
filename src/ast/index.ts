32a277a3b995a7f1ad2947d5648baa49cd8f244d
andoshin11 <shinglish11@gmail.com>
2020-04-10 02:00:38 +0900

    Update ast util

--- src/ast/index.ts
+++ src/ast/index.ts
@@ -1,14 +1,16 @@
 import _ts from 'typescript'
 
 // Find very first node that matches the given syntax kind
-export function findTargetSyntaxKind(ts: typeof _ts, node: _ts.Node, syntaxKind: _ts.SyntaxKind) {
-  function find(node: _ts.Node): _ts.Node | undefined {
+export function findTargetSyntaxKind<SK extends _ts.SyntaxKind>(ts: typeof _ts, node: _ts.Node, syntaxKind: SK) {
+  type TargetNode = SK extends _ts.SyntaxKind.ExportAssignment ? _ts.ExportAssignment : SK extends _ts.SyntaxKind.ObjectLiteralExpression ? _ts.ObjectLiteralExpression : _ts.Node
+
+  function find(node: _ts.Node): TargetNode | undefined {
     if (node.kind === syntaxKind) {
-      return node
+      return node as any
     }
 
     return ts.forEachChild(node, find)
   }
 
-  return find(node)
+  return find(node) as undefined | TargetNode
 }
