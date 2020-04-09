72252d2df393e5d9f2e5ff4b36adec7d3ac6a1d6
andoshin11 <shinglish11@gmail.com>
2020-04-10 03:11:49 +0900

    Update reporter

--- src/reporter/reporter.ts
+++ src/reporter/reporter.ts
@@ -1,10 +1,14 @@
 import _ts from 'typescript'
+import * as fs from 'fs'
 import chalk from 'chalk'
-import { pos2location, hasDiagRange, toRelativePath } from '../helpers'
-import { DiagnosticWithRange } from '../types'
+import { pos2location, hasDiagRange, toRelativePath, isTSVueFile, toRawVueFileName } from '../helpers'
+import { DiagnosticWithRange, SourcemapEntry } from '../types'
 import { lineMark, pad, lineMarkForUnderline } from './helper'
+import { getOriginalPositionFor } from '../sourcemap'
 
 export class Reporter {
+  constructor(private sourcemapEntry: SourcemapEntry) {}
+
   report(msg: any) {
     console.log(msg)
   }
@@ -38,20 +42,40 @@ export class Reporter {
 
   _reportDiagnosticWithRange(diagnostic: DiagnosticWithRange) {
     const { start, length, file, messageText } = diagnostic
-    const content = file && file.getFullText()
+    let content = file && file.getFullText()
     if (!content) return
 
+    const fileName = file!.fileName
+
+    let startLC = pos2location(content, start)
+    let endLC = pos2location(content, start + length)
+
+    /**
+     * If diagnostic target is .vue file,
+     * then recover original position from sourcefile.
+     */
+    if (isTSVueFile(fileName)) {
+      startLC = getOriginalPositionFor(fileName, startLC, this.sourcemapEntry)!
+      endLC = getOriginalPositionFor(fileName, endLC, this.sourcemapEntry)!
+      content = fs.readFileSync(toRawVueFileName(fileName), 'utf8')
+    }
+
+    /**
+     * Example:
+     * > src/App.vue.ts:24:1 - Property 'length' does not exist on type 'number'.
+     */
     const relativeFileName = toRelativePath(file!.fileName)
-    const startLC = pos2location(content, start)
-    const endLC = pos2location(content, start + length)
-    const fileIndicator = `${relativeFileName}:${startLC.line + 1}:${startLC.character + 1}`
+    const fileIndicator = `${relativeFileName}:${startLC.line + 1}:${startLC.column + 1}`
     const message = typeof messageText === 'string' ? messageText : messageText.messageText
     const outputs = [`${fileIndicator} - ${message}`, '']
+
+
     const allLines = content.split('\n');
     const preLines = allLines.slice(Math.max(startLC.line - 1, 0), startLC.line);
     const lines = allLines.slice(startLC.line, endLC.line + 1);
     const postLines = allLines.slice(endLC.line + 1, Math.min(allLines.length - 1, endLC.line + 2));
     const lineMarkerWidth = (Math.min(allLines.length - 1, endLC.line + 2) + '').length;
+    const showUnderLine = lineMarkerWidth > 1
     for (let i = 0; i < preLines.length; ++i) {
       outputs.push(lineMark(i + startLC.line - 1, lineMarkerWidth) + chalk.reset(preLines[i]));
     }
@@ -59,20 +83,22 @@ export class Reporter {
       outputs.push(lineMark(i + startLC.line, lineMarkerWidth) + lines[i]);
       if (i === 0) {
         if (startLC.line === endLC.line) {
-          outputs.push(
-            lineMarkForUnderline(lineMarkerWidth) +
-              pad(' ', startLC.character) +
-              chalk.red(pad('~', endLC.character - startLC.character)),
-          );
+          if (showUnderLine) {
+            outputs.push(
+              lineMarkForUnderline(lineMarkerWidth) +
+                pad(' ', startLC.column) +
+                chalk.red(pad('~', endLC.column - startLC.column)),
+            );
+          }
         } else {
           outputs.push(
             lineMarkForUnderline(lineMarkerWidth) +
-              pad(' ', startLC.character) +
-              chalk.red(pad('~', lines[i].length - startLC.character)),
+              pad(' ', startLC.column) +
+              chalk.red(pad('~', lines[i].length - startLC.column)),
           );
         }
       } else if (i === lines.length - 1) {
-        outputs.push(lineMarkForUnderline(lineMarkerWidth) + chalk.red(pad('~', endLC.character)));
+        outputs.push(lineMarkForUnderline(lineMarkerWidth) + chalk.red(pad('~', endLC.column)));
       } else {
         outputs.push(lineMarkForUnderline(lineMarkerWidth) + chalk.red(pad('~', lines[i].length)));
       }
