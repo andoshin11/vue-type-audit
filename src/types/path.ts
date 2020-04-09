38f3fcfb5c7034f3c880d43fec6ddf14692b5c0b
andoshin11 <shinglish11@gmail.com>
2020-04-10 01:57:34 +0900

    Add path type

---
+++ src/types/path.ts
@@ -0,0 +1,17 @@
+import { Brand } from '.'
+
+export type RawVueFileName = Brand<string, 'RawVueFileName'>
+
+export type TSVueFileName = Brand<string, 'TSVueFileName'>
+
+export type VueTemplateFileName = Brand<string, 'VueTemplateFileName'>
+
+export type VueScriptFileName = Brand<string, 'VueScriptFileName'>
+
+export type CompiledVueTemplateFileName = Brand<string, 'CompiledVueTemplateFileName'>
+
+export type CompiledVueScriptFileName = Brand<string, 'CompiledVueTemplateFileName'>
+
+export type TransformedCompiledVueTemplateFileName = Brand<string, 'TransformedCompiledVueTemplateFileName'>
+
+export type TransformedVueScriptFileName = Brand<string, 'TransformedVueScriptFileName'>
