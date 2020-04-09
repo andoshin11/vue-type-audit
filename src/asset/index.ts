986c076bf782566347db609e3aebd85f58f7c021
andoshin11 <shinglish11@gmail.com>
2020-04-10 01:59:55 +0900

    Update Asset

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,10 +1,25 @@
 export const GLOBAL_TYPES_FILE = {
   name: '__GLOBAL_TYPES.d.ts',
   content: `
-  import { ComponentPublicInstance } from 'vue'
+import { ComponentPublicInstance, VNodeProps, VNode, VNodeTypes, ComponentOptions } from 'vue'
 
-  export type ClassInstance<T> = T extends new (...args: any[]) => infer U ? U : never;
+export type ClassInstance<T> = T extends new (...args: any[]) => infer U ? U : never;
 
-  export type PropTyps<C> = C extends ComponentPublicInstance<infer P, infer B, infer D, infer C, infer M> ? P : never
+export type PropTypes<C> = C extends ComponentPublicInstance<infer P, infer B, infer D, infer C, infer M> ? P : never
+
+export declare function __createVNodeFromExternalComponent<C>(type: unknown, ...args: ({} extends PropTypes<C> ? [(VNodeProps & PropTypes<C>)?, unknown?, number?, (string[] | null)?] : [(VNodeProps & PropTypes<C>), unknown?, number?, (string[] | null)?])): VNode;
+
+declare function _createNode<D, N extends keyof D, C = D[N]>(type: N, ...args: ({} extends PropTypes<C> ? [(VNodeProps & PropTypes<C>)?, unknown?, number?, (string[] | null)?] : [(VNodeProps & PropTypes<C>), unknown?, number?, (string[] | null)?])): VNode;
+
+export type _VNodeProps = VNodeProps
+export type _VNode = VNode
+export type _VNodeTypes = VNodeTypes
+export interface _ClassComponent {
+  new (...args: any[]): ComponentPublicInstance<any, any, any, any, any>;
+  __vccOpts: ComponentOptions;
+}
+export type _Data = {
+  [key: string]: unknown;
+};
 `
 }
