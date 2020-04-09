44319f8b392c4ce8962b5bcdf923d1a8b3d5813c
andoshin11 <shinglish11@gmail.com>
2020-04-10 02:33:19 +0900

    Update Language Service Host

--- src/languageService/createHost.ts
+++ src/languageService/createHost.ts
@@ -1,9 +1,16 @@
 import _ts from 'typescript'
 import * as fs from 'fs'
 import * as path from 'path'
-import { FileEntry } from '../types'
+import { FileEntry, SourcemapEntry } from '../types'
+import { transformVueFile } from '../transformers'
+import { Printer } from '../printer'
+import { GLOBAL_TYPES_FILE } from '../asset'
+import { isRawVueFile, isTSVueFile, toRawVueFileName } from '../helpers'
 
-export const createHost = (fileNames: string[], compilerOptions: _ts.CompilerOptions, fileEntry: FileEntry, ts: typeof _ts): _ts.LanguageServiceHost => {
+export const createHost = (fileNames: string[], compilerOptions: _ts.CompilerOptions, fileEntry: FileEntry, sourcemapEntry: SourcemapEntry, printer: Printer, ts: typeof _ts): _ts.LanguageServiceHost => {
+  // Register GLOBAL_TYPES_FILE
+  fileNames = [...fileNames, GLOBAL_TYPES_FILE.name]
+
   const getCurrentVersion = (fileName: string) => fileEntry.has(fileName) ? fileEntry.get(fileName)!.version : 0
   const getTextFromSnapshot = (snapshot: _ts.IScriptSnapshot) => snapshot.getText(0, snapshot.getLength())
 
@@ -28,6 +35,10 @@ export const createHost = (fileNames: string[], compilerOptions: _ts.CompilerOpt
       return ts.sys.fileExists(fileName) || readFile(fileName) !== undefined
     },
     readFile: fileName => {
+      if (fileName === GLOBAL_TYPES_FILE.name) {
+        return GLOBAL_TYPES_FILE.content
+      }
+
       if (fileEntry.has(fileName)) {
         const snapshot = fileEntry.get(fileName)!.scriptSnapshot
         return getTextFromSnapshot(snapshot)
@@ -44,13 +55,28 @@ export const createHost = (fileNames: string[], compilerOptions: _ts.CompilerOpt
     getScriptFileNames: () => fileNames,
     getScriptVersion: fileName => getCurrentVersion(fileName) + '',
     getScriptSnapshot: fileName => {
+      if (fileName === GLOBAL_TYPES_FILE.name) {
+        const scriptSnapshot = ts.ScriptSnapshot.fromString(GLOBAL_TYPES_FILE.content)
+        fileEntry.set(fileName, { version: 0, scriptSnapshot })
+        return scriptSnapshot
+      }
+
+      // recover file name
+      if (isTSVueFile(fileName)) {
+        fileName = toRawVueFileName(fileName)
+      }
+
       if (fileEntry.has(fileName)) {
         return fileEntry.get(fileName)!.scriptSnapshot
       } else {
         if (!fs.existsSync(fileName)) {
           return undefined
         }
-        const content = fs.readFileSync(fileName).toString()
+        let content = fs.readFileSync(fileName).toString()
+
+        if (isRawVueFile(fileName)) {
+          content = transformVueFile(fileName, content, sourcemapEntry)
+        }
 
         const scriptSnapshot = ts.ScriptSnapshot.fromString(content)
         fileEntry.set(fileName, { version: 0, scriptSnapshot })
@@ -62,6 +88,13 @@ export const createHost = (fileNames: string[], compilerOptions: _ts.CompilerOpt
     getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
     resolveModuleNames: (moduleNames, containingFile, _, __, options) => {
       const ret: (_ts.ResolvedModule | undefined)[] = moduleNames.map(name => {
+        if (name === GLOBAL_TYPES_FILE.name.replace('.d.ts', '')) {
+          const resolved: _ts.ResolvedModule = {
+            resolvedFileName: GLOBAL_TYPES_FILE.name
+          }
+          return resolved
+        }
+
           if (/\.vue$/.test(name)) {
             const resolved: _ts.ResolvedModule = {
               resolvedFileName: normalize(path.resolve(path.dirname(containingFile), name))
