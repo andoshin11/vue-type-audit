a4e9d0e815e9bc9808631c3013ba70c40574b2d1
andoshin11 <shinglish11@gmail.com>
2020-04-10 02:03:51 +0900

    Enhance path helpers

--- src/helpers/path.ts
+++ src/helpers/path.ts
@@ -1,1 +1,50 @@
-export const toRelativePath = (str: string) => str.replace(process.cwd() + '/', '')
+import {
+  RawVueFileName,
+  VueTemplateFileName,
+  CompiledVueTemplateFileName,
+  TransformedCompiledVueTemplateFileName,
+  TSVueFileName,
+  VueScriptFileName,
+  TransformedVueScriptFileName
+} from "../types";
+
+export const toRelativePath = (str: string) =>
+  str.replace(process.cwd() + "/", "");
+
+export const toTSVueFIleName = (fileName: RawVueFileName) => `${fileName}.ts` as TSVueFileName
+
+export const toVueTemplateFileName = (fileName: RawVueFileName) =>
+  `${fileName}.template.ts` as VueTemplateFileName;
+
+export const toVueScriptFileName = (fileName: RawVueFileName) =>
+  `${fileName}.script.ts` as VueScriptFileName;
+
+export const toCompiledVueTemplateFileName = (fileName: VueTemplateFileName) =>
+  fileName.replace(/\.ts$/, ".compiled.ts") as CompiledVueTemplateFileName;
+
+export const toTransformedCompiledVueTemplateFileName = (
+  fileName: CompiledVueTemplateFileName
+) =>
+  fileName.replace(
+    /\.compiled\.ts$/,
+    ".compiled.transformed.ts"
+  ) as TransformedCompiledVueTemplateFileName;
+
+export const toTransformedVueScriptFileName = (fileName: VueScriptFileName) => fileName.replace(/\.ts$/, '.transformed.ts') as TransformedVueScriptFileName
+
+export const toRawVueFileName = (fileName: TSVueFileName) => fileName.replace(/\.ts$/, '')
+
+/**
+ * User Type Guards
+ */
+export const isRawVueFile = (fileName: string): fileName is RawVueFileName =>
+  /\.vue$/.test(fileName);
+
+export const isTSVueFile = (fileName: string): fileName is TSVueFileName =>
+  /\.vue\.ts$/.test(fileName);
+
+export const isVueScriptFile = (fileName: string): fileName is VueScriptFileName => /\.vue\.script\.ts$/.test(fileName)
+
+export const isVueTemplateFile = (fileName: string): fileName is VueTemplateFileName => /\.vue\.template\.ts$/.test(fileName)
+
+export const isCompiledVueTemplateFile = (fileName: string): fileName is CompiledVueTemplateFileName => /\.vue\.template\.compiled\.ts$/.test(fileName)
