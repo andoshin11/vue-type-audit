51f5dab3a6f01a9884fc6099de5d5dd32c552073
andoshin11 <shinglish11@gmail.com>
2020-04-12 23:40:29 +0900

    Update DiagnosticWithRange type

--- src/types/diagnostics.ts
+++ src/types/diagnostics.ts
@@ -1,3 +1,3 @@
 import _ts from 'typescript'
 
-export type DiagnosticWithRange = Omit<_ts.Diagnostic, 'start' | 'length'> & { start: NonNullable<_ts.Diagnostic['start']>; length: NonNullable<_ts.Diagnostic['length']> }
+export type DiagnosticWithRange = Omit<_ts.Diagnostic, 'start' | 'length' | 'file'> & { start: NonNullable<_ts.Diagnostic['start']>; length: NonNullable<_ts.Diagnostic['length']>; file: NonNullable<_ts.Diagnostic['file']> }
