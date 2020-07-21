efc1a5978d19961981a3bbdd2c78ea3e6fdf92ed
shin-ando <shin.ando@linecorp.com>
2020-07-21 20:46:49 +0900

    Update Type helpers

--- src/transformers/createTypeHelpers.ts
+++ src/transformers/createTypeHelpers.ts
@@ -5,10 +5,10 @@ function camelize(str: string) {
 export function createTypeHelpers(externalComponents: Record<string, string>, eventNames: string[]) {
   return `
 ///////////////////// START: Type Helpers ///////////////////
-import { ClassInstance, _VNodeProps, _resolveComponent, PropTypes, _VNode, _VNodeTypes, _ClassComponent, _Data, RequiredPropNames, WithEmitType, MixIntoComponent } from "__GLOBAL_TYPES";
+import { ClassInstance, _VNodeProps, _resolveComponent, PropTypes, _VNode, _VNodeTypes, _ClassComponent, _Data, RequiredPropNames, WithEmitType, MixIntoComponent, isNeverType } from "__GLOBAL_TYPES";
 
 type EVENT_DICT = {
-${eventNames.map(n => `  on${camelize(n)}: "${n}"`)}
+${eventNames.map(n => `  on${camelize(n)}: "${n}";`).join('\n')}
 }
 
 type __ExternalComponents = {
@@ -22,12 +22,11 @@ ${Object.entries(externalComponents).map(([key, val]) => `  ${key}: RequiredProp
 const ___Component: MixIntoComponent<typeof __Component, WithEmitType<typeof __Component, EVENT_DICT>> = __Component as any
 
 export default ___Component;
-type __ComponentType = ClassInstance<typeof __Component>;
 
 type __PropArg<N> = N extends keyof __ExternalComponents ?  PropTypes<__ExternalComponents[N]> : _Data
 type __EmitArg<N> = N extends keyof __ExternalComponents ? __ExternalComponents[N]['$options']['__emitHandlerTypes'] : {}
 
-declare function _createVNode<N extends (keyof __ExternalComponents | _VNodeTypes | _ClassComponent)>(type: N, ...args: (N extends keyof __ExternalComponents ? [__ExternalComponentsProps[N]] extends [never] ? [(_VNodeProps & __PropArg<N> & __EmitArg<N>)?, unknown?, number?, (string[] | null)?, boolean?] : [(_VNodeProps & __PropArg<N> & __EmitArg<N>), unknown?, number?, (string[] | null)?, boolean?] : [((_Data & _VNodeProps) | null)?, unknown?, number?, (string[] | null)?, boolean?])): _VNode;
+declare function _createVNode<N extends (keyof __ExternalComponents | _VNodeTypes | _ClassComponent)>(type: N, ...args: (N extends keyof __ExternalComponents ? true extends isNeverType<__ExternalComponentsProps[N]> ? [(_VNodeProps & __PropArg<N> & __EmitArg<N>)?, unknown?, number?, (string[] | null)?, boolean?] : [(_VNodeProps & __PropArg<N> & __EmitArg<N>), unknown?, number?, (string[] | null)?, boolean?] : [((_Data & _VNodeProps) | null)?, unknown?, number?, (string[] | null)?, boolean?])): _VNode;
 ///////////////////// END: Type Helpers /////////////////////
 `
 }
