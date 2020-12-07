20ab159ad7cc11120f10ea2ca170729af991708e
andoshin11 <shinglish11@gmail.com>
2020-12-07 19:15:03 +0900

    Update e2e

--- test/e2e/specs/audit.test.ts
+++ test/e2e/specs/audit.test.ts
@@ -1,6 +1,8 @@
 import * as path from 'path'
 import ts from 'typescript'
 import { createServiceForTest } from '../service'
+import { translateTSVuefileDiagnostic } from '../../../src/helpers/diagnostics'
+import { isTSVueFile } from '../../../src/helpers'
 
 function diagnosticSummary(diagnostic: ts.Diagnostic) {
   const { category, code, file, start, length, messageText } = diagnostic
@@ -17,9 +19,12 @@ function diagnosticSummary(diagnostic: ts.Diagnostic) {
 
 describe('audit', () => {
   test('should match snapshot', async () => {
+    const localTS = require('../../../node_modules/typescript')
     const configPath = path.resolve(__dirname, '../project/tsconfig.json')
     const service = createServiceForTest(configPath)
-    const diagnostics = service.getSemanticDiagnostics()
+    const diagnostics = service.getNativeSemanticDiagnostics()
     expect(diagnostics.map(diagnosticSummary)).toMatchSnapshot()
+    const translated = diagnostics.map(d => translateTSVuefileDiagnostic(d, service.sourcemapEntry, service.tsService.getProgram()!.getTypeChecker(), localTS))
+    expect(translated.map(diagnosticSummary)).toMatchSnapshot()
   })
 })
